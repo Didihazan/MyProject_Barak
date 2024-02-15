4651aef5668adb9722ccc621f7496d6f02d6d72e
Yedidya <didihazan123@gmail.com>
2024-02-15 14:02:24 +0200

    Start

---
+++ myFunction.c
@@ -0,0 +1,77 @@
+#include "myFunction.h"
+
+// בפונקציה הנ"ל קיבלנו את הנתיב ממנו אנחנו מריצים את התוכנית שלנו
+//  עליכם לשדרג את הנראות של הנתיב כך ש-בתחילת הנתיב יופיע שם המחשב (כמו בטרמינל המקורי) בסוף יופיע הסימן דולר
+//  ולאחר הדולר ניתן אפשרות למשתמש להזין מחרוזת מבלי שנרד שורה.
+void getLocation()
+{
+    char location[SIZE_BUFF];
+    if (getcwd(location, SIZE_BUFF) == NULL)
+        printf("Error\n");
+    else
+    {
+        printf("\033[0;34m");
+        puts(location);
+        printf("\033[0m");
+    }
+}
+
+char *getInputFromUser()
+{
+    char ch;
+    int size = 1;
+    int index = 0;
+    char *str = (char *)malloc(size * sizeof(char));
+    while ((ch = getchar()) != '\n')
+    {
+        *(str + index) = ch;
+        size++;
+        index++;
+        str = (char *)realloc(str, size);
+    }
+    *(str + index) = '\0';
+    return str;
+}
+
+// עליכם לממש את הפונקציה strtok כלומר שהפונקציה הנ"ל תבצע בדיוק אותו הדבר רק בלי השימוש בפונקציה strtok
+char **splitArgument(char *str)
+{
+    // cp file file
+    //[cp,file,file,NULL]
+    char *subStr;
+    // cp\0file file
+    // hello1\0hello2\0hello3\0hello4\0
+    // subStr = address of 'h'
+    // str = address of 'h'
+    // int i=0;
+    // int startIndex=0;
+    // while(*str+i!=' '){
+    // }
+    // *(str+i)='\0';
+    // // [str+startIndex,]
+    // startIndex=++i;
+    // while(*str+i!=' '){
+    // }
+    // *(str+i)='\0';
+    // // [str+startIndex,str+startIndex]
+    // startIndex=++i;
+
+    subStr = strtok(str, " ");
+    int size = 2;
+    int index = 0;
+    char **argumnts = (char **)malloc(size * sizeof(char *));
+    *(argumnts + index) = subStr; // [subStr,subStr,subStr,subStr,NULL]
+    while ((subStr = strtok(NULL, " ")) != NULL)
+    {
+        index++;
+        size++;
+        argumnts = (char **)realloc(argumnts, size * sizeof(char *));
+        *(argumnts + index) = subStr;
+    }
+    *(argumnts + (index + 1)) = NULL;
+
+    return argumnts;
+}
+
+// בכל שינוי יש לבצע קומיט מתאים העבודה מחייבת עבודה עם גיט.
+// ניתן להוסיף פונקציות עזר לתוכנית רק לשים לב שלא מוסיפים את חתימת הפונקציה לקובץ הכותרות
---
+++ myShell.c
@@ -0,0 +1,47 @@
+#include "myShell.h"
+#include "myFunction.h"
+int main()
+{
+    welcome();
+    while (1)
+    {
+        getLocation();
+        char *input = getInputFromUser();
+        puts(input);
+        char **arguments = splitArgument(input);
+        for (int i = 0; *(arguments + i) != NULL; i++)
+        {
+            puts(*(arguments+i));
+            puts(arguments[i]);
+        }
+
+        free(arguments);
+        free(input);
+
+        break;
+    }
+    return 0;
+}
+// יש לכתוב את פונקציית הברוכים הבאים כרצונכם אבל קצת יותר ממה שמוצג מטה לדוגמא:
+//                     aSPY//YASa       
+//              apyyyyCY//////////YCa       |
+//             sY//////YSpcs  scpCY//Pp     | Welcome to myShell
+//  ayp ayyyyyyySCP//Pp           syY//C    | Version 2.4.3
+//  AYAsAYYYYYYYY///Ps              cY//S   |
+//          pCCCCY//p          cSSps y//Y   | https://github.com/<user>
+//          SPPPP///a          pP///AC//Y   |
+//               A//A            cyP////C   | Have fun!
+//               p///Ac            sC///a   |
+//               P////YCpc           A//A   | Craft packets like it is your last
+//        scccccp///pSP///p          p//Y   | day on earth.
+//       sY/////////y  caa           S//P   |                      -- Lao-Tze
+//        cayCyayP//Ya              pY/Ya   |
+//         sY/PsY////YCc          aC//Yp 
+//          sc  sccaCY//PCypaapyCP//YSs  
+//                   spCPY//////YPSps    
+//                        ccaacs         
+//                                        using c 
+void welcome()
+{
+    puts("Welcome to my Shell");
+}
