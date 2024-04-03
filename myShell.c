cf7ded48c14f42b4893007807725f1056043e67d
Yedidya Hazan <109093124+Didihazan@users.noreply.github.com>
2024-04-03 08:05:22 +0300

    Update myShell.c

--- myShell.c
+++ myShell.c
@@ -3,45 +3,54 @@
 
 
 
-void welcome();
+ bool piping = false;
+char ***splitByPipe(char **args)
+{
+    int count1 =0;
+    int size1 = 2, size2 = 2;
+    char ***result = (char ***)malloc(sizeof(char **)*3);
+    result[0] = (char **)malloc(sizeof(char *) * size1);
+    result[1] = (char **)malloc(sizeof(char *) * size2);
+    int isPipe = 0;
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], "|") == 0)
+        {
+            isPipe = 1;
+            continue;
+        }
+        if (!isPipe)
+        {
+            result[0][count1] = args[i];
+            count1++;
+            if (count1 >= size1)
+            {
+                size1 *= 2;
+                result[0] = (char **)realloc(result[0], sizeof(char *) * size1);
+            }
+        }
+        else
+        {
+            result[1][count2] = args[i];
+            count2++;
+            if (count2 >= size2)
+            {
+                size2 *= 2;
+                result[1] = (char **)realloc(result[1], sizeof(char *) * size2);
+            }
+        }
+    }
+    result[0][count1] = NULL;
+    result[1][count2] = NULL;
+    return result;
+}
 
 int main() {
+
     welcome();
-
-
-    while (1) {
-        char hostname[256];
-        struct utsname unameData;
-
-        // קבלת שם המחשב באמצעות הפונקציה gethostname
-        if (gethostname(hostname, sizeof(hostname)) == 0) {
-            printf("Hostname: %s\n", hostname); // הדפסת שם המחשב
-        } else {
-            perror("gethostname");
-            return 1;
-        }
-
-        // קבלת גרסת Ubuntu באמצעות הפונקציה uname
-        if (uname(&unameData) == 0) {
-            printf("Ubuntu Version: %s\n", unameData.release); // הדפסת גרסת Ubuntu
-        } else {
-            perror("uname");
-            return 1;
-        }
-         
-      printf("GitHub Username:\n");
-        printf("__   __       _ _     _             \n");
-        printf("\\ \\ / /__  __| (_) __| |_   _  __ _ \n");
-        printf(" \\ V / _ \\/ _` | |/ _` | | | |/ _` |\n");
-        printf("  | |  __/ (_| | | (_| | |_| | (_| |\n");
-        printf("  |_|\\___|\\__,_|_|\\__,_|\\__, |\\__,_|\n");
-        printf("                        |___/       \n");
-        printf("\n");
-
-        break;
-    }
  while (1)
     {
+        bool piping = false;
         errno = 0;
         getLocation();
         char *input = getInputFromUser();
@@ -64,6 +73,42 @@ int main() {
             cp(arguments);
         free(arguments);
         free(input);
+        if(strcmp(arguments[0], "read") == 0){
+            readFile(arguments);
+        }
+        else if(strcmp(arguments[0], "echo") == 0){
+            echorite(arguments);
+        }
+        else if(strcmp(arguments[0], "wc") == 0){
+            wordCount(arguments);
+        }
+        else if(strcmp(arguments[0], "echo") == 0){
+            echoppend(arguments);
+        }
+        else if(strcmp(arguments[0], "mv") == 0){
+            move(arguments);
+        }
+        else if(strcmp(arguments[0], "delete") == 0){
+            delete(arguments);
+        }
+        else if(strcmp(arguments[0], "dir") == 0){
+            get_dir();
+        }
+         else if (piping)
+        {
+            // indexPipe
+            // arguments[indexPipe]=NULL;
+            // mypipe(arguments, arguments+indexPipe+1);
+            mypipe(arguments, arguments+/*indexPipe*/+1);
+            wait(NULL);
+        }
+        else
+        {
+            systemCall(arguments);
+            wait(NULL);
+        }
+        free(arguments);
+        free(input);
     }
     return 0;
 }
@@ -71,4 +116,12 @@ int main() {
 
 void welcome() {
     printf("Welcome to my Shell\n");
+          printf("GitHub Username:\n");
+        printf("__   __       _ _     _             \n");
+        printf("\\ \\ / /__  __| (_) __| |_   _  __ _ \n");
+        printf(" \\ V / _ \\/ _` | |/ _` | | | |/ _` |\n");
+        printf("  | |  __/ (_| | | (_| | |_| | (_| |\n");
+        printf("  |_|\\___|\\__,_|_|\\__,_|\\__, |\\__,_|\n");
+        printf("                        |___/       \n");
+        printf("\n");
 }
