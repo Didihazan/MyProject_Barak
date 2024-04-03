0e8e371bd08c1917d9e42439add402a7d447c6ef
Yedidya <didihazan123@gmail.com>
2024-04-03 12:51:26 +0300

    fjdjd

--- myFunction.c
+++ myFunction.c
@@ -223,6 +223,32 @@ void systemCall(char **arguments)
 }
 
 
+void mypipe(char **argv1,char ** argv2){
+
+    int fildes[2];
+    if (fork() == 0)
+    {
+        pipe(fildes);
+        if (fork() == 0)
+        {
+            /* first component of command line */
+            close(STDOUT_FILENO);
+            dup(fildes[1]);
+            close(fildes[1]);
+            close(fildes[0]);
+            execvp(argv1[0], argv1);
+        }
+        /* 2nd command component of command line */
+        close(STDIN_FILENO);
+        dup(fildes[0]);
+        close(fildes[0]);
+        close(fildes[1]);
+        /* standard input now comes from pipe */
+        execvp(argv2[0], argv2);
+    }
+}
+
+
 void move(char **args) {
     if (rename(args[0], args[1]) != 0) {
         printf("Cannot move file.\n");
