ae337d0f7ccb8b909088257ca36cdb3600514854
Yedidya <didihazan123@gmail.com>
2024-04-03 12:39:27 +0300

    sasww

--- myFunction.c
+++ myFunction.c
@@ -1,26 +1,35 @@
 #include "myFunction.h"
 
 
-void getLocation()  
+void getLocation()  //פונקציה להצגת המיקום הנוכחי
 {
+    // מגדיר משתנים של מחרוזות שיכילו את המיקום והשם של המחשב
     char location[SIZE_BUFF];
     char hostname[SIZE_BUFF]; 
     char* username;
 
+    // משיג את שם המשתמש מהמשתנה הסביבתי "USER"
     username = getenv("USER");
     if (username == NULL)
+        // אם המשתנה הסביבתי "USER" לא קיים, מדפיס שגיאה
         printf("Error\n");
     else
     {
+        // משיג את המיקום הנוכחי של התיקייה בה אנחנו נמצאים
         if (getcwd(location, SIZE_BUFF) == NULL)
+            // אם לא הצליח לשיג את המיקום, מדפיס שגיאה
             printf("Error\n");
         else
         {
+            // משיג את שם המחשב
             if (gethostname(hostname, SIZE_BUFF) == -1)
+                // אם לא הצליח לשיג את שם המחשב, מדפיס שגיאה
                 printf("Error\n");
             else
             {
+                // מדפיס את שם המשתמש, שם המחשב והמיקום הנוכחי בצורה מסוימת
                 printf("\033[1;34m%s@%s\033[0m:\033[0;32m%s\033[0m$ ", username, hostname, location); 
+                // פורט את הפלאש ומנקה את הזיכרון
                 fflush(stdout);  
             }
         }
@@ -28,106 +37,91 @@ void getLocation()
 }
 
 
-char *getInputFromUser()
+char *getInputFromUser()//קליטת קלט מהמשתמש
 {
     char ch;
     int size = 1;
     int index = 0;
-    char *str = (char *)malloc(size * sizeof(char));
-    while ((ch = getchar()) != '\n')
+    char *str = (char *)malloc(size * sizeof(char));//הקצאת זיכרון למחרוזת
+    while ((ch = getchar()) != '\n')//קליטת תווים מהמשתמש עד לחיצת אנטר
     {
-        *(str + index) = ch;
-        size++;
-        index++;
-        str = (char *)realloc(str, size);
+        *(str + index) = ch;//הכנסת התו למחרוזת
+        size++;//הגדלת המערך
+        index++;//הגדלת האינדקס
+        str = (char *)realloc(str, size);//הרחבת המערך
     }
-    *(str + index) = '\0';
-    return str;
+    *(str + index) = '\0';//הוספת סיומת למחרוזת
+    return str;//החזרת המחרוזת
 
 }
 
-    // hello1\0hello2\0hello3\0hello4\0
-    // subStr = address of 'h'
-    // str = address of 'h'
-    // int i=0;
-    // int startIndex=0;
-    // while(*str+i!=' '){
-    // }
-    // *(str+i)='\0';
-    // // [str+startIndex,]
-    // startIndex=++i;
-    // while(*str+i!=' '){
-    // }
-    // *(str+i)='\0';
-    // // [str+startIndex,str+startIndex]
-    // startIndex=++i;
 
-char **splitArgument(char *str) {
+char **splitArgument(char *str) {//פיצול מחרוזת למערך של מחרוזות
     int size = 2;
     int index = 0;
-    char *start = str;
-    char *end;
-    char **arguments = (char **)malloc(size * sizeof(char *));
-    if (arguments == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
+    char *start = str;//הצבת המצביע לתחילת המחרוזת
+    char *end;//הצבת המצביע לסיום המחרוזת
+    char **arguments = (char **)malloc(size * sizeof(char *));//הקצאת זיכרון למערך של מחרוזות
+    if (arguments == NULL) {//בדיקה האם הקצאת הזיכרון הצליחה
+        perror("malloc");//הדפסת שגיאה
+        exit(EXIT_FAILURE);//יציאה מהתוכנית
     }
 
-    while (*start != '\0') {
-        if (*start == '"') {
-            end = strchr(start + 1, '"');
-            if (end == NULL) {
-                perror("missing \"");
-                errno = EINVAL;
+    while (*start != '\0') {//לולאה שרצה עד שהגענו לסוף המחרוזת
+        if (*start == '"') {//בדיקה האם התו הנוכחי הוא גרש
+            end = strchr(start + 1, '"');//מציאת הגרש הבא
+            if (end == NULL) {//בדיקה האם לא נמצא גרש נוסף
+                perror("missing \"");//הדפסת שגיאה
+                errno = EINVAL;//הגדרת שגיאה
                 break;
             }
-            start++;
-            *end = '\0';
-            if (*(end + 1) == ' ') {
-                end += 2;
-            } else if (*(end + 1) != '\0') {
-                perror("missing space after \"");
-                 errno = EINVAL;
+            start++;//הזזת המצביע לאחר הגרש
+            *end = '\0';//החלפת הגרש בסיומת
+            if (*(end + 1) == ' ') {//בדיקה האם אחרי הגרש יש רווח
+                end += 2;//הזזת המצביע לאחר הרווח
+            } else if (*(end + 1) != '\0') {//בדיקה האם אחרי הגרש יש תו אחר
+                perror("missing space after \"");//הדפסת שגיאה
+                 errno = EINVAL;//הגדרת שגיאה
                 break;
             } else {
-                end++;
+                end++;//הזזת המצביע לתו הבא
             }
         } else {
-            end = strchr(start, ' ');
-            if (end != NULL) {
-                *end = '\0';
-                end++;
+            end = strchr(start, ' ');//מציאת הרווח הבא
+            if (end != NULL) {//בדיקה האם נמצא רווח
+                *end = '\0';//החלפת הרווח בסיומת
+                end++;//הזזת המצביע לתו הבא
             } else {
-                // Handle the case when there is no space at the end of the string
-                end = start + strlen(start);
+            
+                end = start + strlen(start);//הזזת המצביע לסוף המחרוזת
             }
         }
-        arguments[index] = start;
-        index++;
-        if (index >= size) {
-            size += 2;
-            arguments = (char **)realloc(arguments, size * sizeof(char *));
-            if (arguments == NULL) {
+        arguments[index] = start;//הכנסת המחרוזת למערך
+        index++;//הגדלת האינדקס
+        if (index >= size) {//בדיקה האם הגענו לגודל המערך
+            size += 2;//הגדלת המערך
+            arguments = (char **)realloc(arguments, size * sizeof(char *));//הרחבת המערך
+            if (arguments == NULL) {//בדיקה האם הרחבת המערך הצליחה
                 perror("realloc");
                 exit(EXIT_FAILURE);
             }
         }
-        arguments[index] = NULL; // Null-terminate the array
-        start = end;
+        arguments[index] = NULL; //הכנסת סיומת למערך
+        start = end;//הזזת המצביע לתחילת המחרוזת הבאה
     }
     return arguments;
 }
 
 
-void logout(char *input)
+void logout(char *input)//פונקציה ליציאה מהתוכנית
 {
     free(input);
     puts("logout");
-    exit(EXIT_SUCCESS); // EXIT_SUCCESS = 0
+    exit(EXIT_SUCCESS); //יציאה מהתוכנית
 }
 
 
-void echo(char **arguments)
+void echo(char **arguments)//פונקציה להדפסת מחרוזות
 {
 
     // int i = 1;
@@ -137,172 +131,173 @@ void echo(char **arguments)
     // while (arguments[i])
     //     printf("%s ", arguments[i]);
 
-    while (*(++arguments))
-        printf("%s ", *arguments);
+    while (*(++arguments))//הדפסת המחרוזות שנמצאות במערך
+        printf("%s ", *arguments);//הדפסת המחרוזת
 
-    puts("");
+    puts("");//הדפסת שורה חדשה
 }
 
 
-void cd(char **path) {
-    if (path[2] != NULL) {
+void cd(char **path) {//פונקציה לשינוי תיקייה
+    if (path[2] != NULL) {//בדיקה האם יש יותר מדי ארגומנטים
         printf("-myShell: cd: too many arguments\n");
         return;
     } 
-     if (chdir(path[1]) != 0)  
+     if (chdir(path[1]) != 0)  //בדיקה האם הצליח לשנות את התיקייה
         printf("-myShell: cd: %s: No such file or directory\n", path[1]);
 }
 
 
-void cp(char **arguments)
+void cp(char **arguments)//פונקציה להעתקת קובץ
 {
-    if(arguments[1]==NULL || arguments[2]==NULL){
+    if(arguments[1]==NULL || arguments[2]==NULL){//בדיקה האם יש מספר לא תקין של ארגומנטים
         puts("error");
         return;
     }
-    if(arguments[3]!=NULL){
+    if(arguments[3]!=NULL){//בדיקה האם יש מספר לא תקין של ארגומנטים
         puts("error");
         return;
     }
     char ch;
-    FILE *src, *des;
-    if ((src = fopen(arguments[1], "r")) == NULL)//open file source
+    FILE *src, *des;//הגדרת משתנים לקבצים
+    if ((src = fopen(arguments[1], "r")) == NULL)//פתיחת קובץ מקור
     {
         puts("error");
         return;
     }
 
-    if ((des = fopen(arguments[2], "w")) == NULL)//open file destination
+    if ((des = fopen(arguments[2], "w")) == NULL)//פתיחת קובץ יעד
     {
         puts("error");
-        fclose(src);
+        fclose(src);//סגירת הקובץ המקורי
         return;
     }
-    while ((ch = fgetc(src)) != EOF)//copy the file
-        fputc(ch, des);
+    while ((ch = fgetc(src)) != EOF)//העתקת התוכן מהקובץ המקורי לקובץ היעד
+        fputc(ch, des);//הכנסת התו לקובץ היעד
 
     fclose(src);
     fclose(des);
 }
 
-void get_dir()
+
+void get_dir()//פונקציה להצגת קבצים בתיקייה
 {
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir("./")) == NULL)
+    DIR *dir;//משתנה לתיקייה
+    struct dirent *ent;//משתנה לקבצים בתיקייה
+    if ((dir = opendir("./")) == NULL)//פתיחת התיקייה
     {
         perror("");
         return;
     }
-    while ((ent = readdir(dir)) != NULL)
-        printf("%s ", ent->d_name);
+    while ((ent = readdir(dir)) != NULL)//הדפסת קבצים בתיקייה
+        printf("%s ", ent->d_name);//הדפסת שם הקובץ
     puts("");
 }
 
 
-void delete(char **path)
+void delete(char **path)//פונקציה למחיקת קובץ
 {
-    if (unlink(path[1]) != 0)
+    if (unlink(path[1]) != 0)//בדיקה האם הצליח למחוק את הקובץ
         printf("-myShell: delete: %s: No such file or directory\n", path[1]);
 }
 
 
-void systemCall(char **arguments)
+void systemCall(char **arguments)//פונקציה להפעלת תוכניות
 {
-    pid_t pid = fork();
-    if (pid == -1)
+    pid_t pid = fork();//יצירת תהליך ילד
+    if (pid == -1)//בדיקה האם הצליח ליצור תהליך
     {
         printf("fork err\n");
         return;
     }
-    if (pid == 0)
+    if (pid == 0)//בדיקה האם זה התהליך הילד
     {
-        if (execvp(arguments[0], arguments) == -1)
-            exit(EXIT_FAILURE);
+        if (execvp(arguments[0], arguments) == -1)//הפעלת התוכנית
+            exit(EXIT_FAILURE);//יציאה מהתהליך
     }
 }
 
 
-void move(char **args) {
-    if (rename(args[0], args[1]) != 0) {
+void move(char **args) {//פונקציה להעברת קובץ
+    if (rename(args[0], args[1]) != 0) {//בדיקה האם הצליח להעביר את הקובץ
         printf("Cannot move file.\n");
     }
 }
 
 
-void echoppend(char **args) {
-    FILE *file;
+void echoppend(char **args) {//פונקציה להוספת מחרוזת לקובץ
+    FILE *file;//משתנה לקובץ
 
-    file = fopen(args[1], "a");
-    if (file == NULL) {
+    file = fopen(args[1], "a");//פתיחת קובץ לכתיבה
+    if (file == NULL) {//בדיקה האם הצליח לפתוח את הקובץ
         printf("Cannot open file.\n");
         return;
     }
 
-    fprintf(file, "%s", args[0]);
+    fprintf(file, "%s", args[0]);//הוספת המחרוזת לקובץ
 
     fclose(file);
 }
 
 
-void echorite(char **args) {
-    FILE *file;
+void echorite(char **args) {//פונקציה לכתיבת מחרוזת לקובץ
+    FILE *file;//משתנה לקובץ
 
-    file = fopen(args[1], "w");
-    if (file == NULL) {
+    file = fopen(args[1], "w");//פתיחת קובץ לכתיבה
+    if (file == NULL) {//בדיקה האם הצליח לפתוח את הקובץ
         printf("Cannot open file.\n");
         return;
     }
 
-    fprintf(file, "%s", args[0]);
+    fprintf(file, "%s", args[0]);//הוספת המחרוזת לקובץ
 
     fclose(file);
 }
 
 
-void readFile(char **args) {
-    FILE *file;
+void readFile(char **args) {//פונקציה לקריאת קובץ
+    FILE *file;//משתנה לקובץ
     char ch;
 
-    file = fopen(args[0], "r");
-    if (file == NULL) {
+    file = fopen(args[0], "r");//פתיחת קובץ לקריאה
+    if (file == NULL) {//בדיקה האם הצליח לפתוח את הקובץ
         printf("Cannot open file.\n");
         return;
     }
 
-    while ((ch = fgetc(file)) != EOF) {
-        putchar(ch);
+    while ((ch = fgetc(file)) != EOF) {//הדפסת הקובץ
+        putchar(ch);//הדפסת התו
     }
 
     fclose(file);
 }
 
 
-void wordCount(char **args) {
-    FILE *file;
+void wordCount(char **args) {//פונקציה לספירת מילים ושורות בקובץ
+    FILE *file;//משתנה לקובץ
     char ch;
-    int lines = 0, words = 0, characters = 0;
+    int lines = 0, words = 0, characters = 0;//משתנים לספירת שורות, מילים ותווים
 
-    file = fopen(args[1], "r");
-    if (file == NULL) {
+    file = fopen(args[1], "r");//פתיחת קובץ לקריאה
+    if (file == NULL) {//בדיקה האם הצליח לפתוח את הקובץ
         printf("Cannot open file.\n");
         return;
     }
 
-    while ((ch = fgetc(file)) != EOF) {
-        characters++;
-        if (ch == '\n' || ch == '\0') {
-            lines++;
+    while ((ch = fgetc(file)) != EOF) {//ספירת שורות, מילים ותווים
+        characters++;//הגדלת מונה התווים
+        if (ch == '\n' || ch == '\0') {//בדיקה האם יש סיום שורה
+            lines++;//הגדלת מונה השורות
         }
-        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0') {
-            words++;
+        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0') {//בדיקה האם יש רווח, טאב או סיום שורה
+            words++;//הגדלת מונה המילים
         }
     }
 
-    if (args[0][1] == 'l') {
-        printf("Lines: %d\n", lines);
-    } else if (args[0][1] == 'w') {
-        printf("Words: %d\n", words);
+    if (args[0][1] == 'l') {//בדיקה האם צריך להדפיס את מספר השורות
+        printf("Lines: %d\n", lines);//הדפסת מספר השורות
+    } else if (args[0][1] == 'w') {//בדיקה האם צריך להדפיס את מספר המילים
+        printf("Words: %d\n", words);//הדפסת מספר המילים
     }
 
     fclose(file);
--- myShell.c
+++ myShell.c
@@ -4,97 +4,102 @@
 
 
  bool piping = false;
-char ***splitByPipe(char **args) {
-    char ***result = (char ***)malloc(sizeof(char **) * 1);
-    int count = 0;
-    int indexResult = 0;
-    int size = 0;
-    for (int i = 0; args[i]; i++) {
-        count++;
-        size++;
-        if (strcmp(args[i], "|") == 0) {
-            piping = true;
-            result = (char ***)realloc(result, sizeof(char **) * (indexResult + 2));
-            result[indexResult] = (char **)malloc(sizeof(char *) * count);
-            for (int j = i - count + 1; j < count - 1; j++) {
-                result[indexResult][j - (i - count + 1)] = args[j];
+char ***splitByPipe(char **args)
+{
+    int count1 =0;
+    int size1 = 2, size2 = 2;
+    char ***result = (char ***)malloc(sizeof(char **)*3);
+    result[0] = (char **)malloc(sizeof(char *) * size1);
+    result[1] = (char **)malloc(sizeof(char *) * size2);
+    int isPipe = 0;
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], "|") == 0)
+        {
+            isPipe = 1;
+            continue;
+        }
+        if (!isPipe)
+        {
+            result[0][count1] = args[i];
+            count1++;
+            if (count1 >= size1)
+            {
+                size1 *= 2;
+                result[0] = (char **)realloc(result[0], sizeof(char *) * size1);
             }
-            result[indexResult][count - 1] = NULL;
-            indexResult++;
-            count = 0;
+        }
+        else
+        {
+            result[1][count2] = args[i];
+            count2++;
+            if (count2 >= size2)
+            {
+                size2 *= 2;
+                result[1] = (char **)realloc(result[1], sizeof(char *) * size2);
+            }
         }
     }
-
-    // Handle last substring
-    if (piping) {
-        result = (char ***)realloc(result, sizeof(char **) * (indexResult + 2));
-        result[indexResult] = (char **)malloc(sizeof(char *) * (count + 1));
-        for (int j = size - count; j < size; j++) {
-            result[indexResult][j - (size - count)] = args[j];
-        }
-        result[indexResult][count] = NULL;
-        indexResult++;
-        result[indexResult] = NULL;
-    } else {
-        // If no pipe found, free and return NULL
-        free(result);
-        return NULL;
-    }
-
+    result[0][count1] = NULL;
+    result[1][count2] = NULL;
     return result;
 }
 
-int main() {
+int main() {//פונקצית ראשית
 
     welcome();
- while (1)
+ while (1)//לולאת אינפינטי
     {
-    piping = false;
+        bool piping = false;
         errno = 0;
         getLocation();
         char *input = getInputFromUser();
         if (strcmp(input, "exit") == 0 || strncmp(input, "exit ", 5) == 0)
             logout(input);
 
-        char **arguments = splitArgument(input);
-         if(strcmp(arguments[0], "exit") == 0){ 
+        char **arguments = splitArgument(input);//קריאה לפונקציה לפיצול המחרוזת למערך של מחרוזות
+         if(strcmp(arguments[0], "exit") == 0){ //בדיקה האם המחרוזת הראשונה במערך היא "exit"
             free(arguments);
             logout(input);
 }
-char ***splitArgumentsArr = splitByPipe(arguments);
- if( errno !=0) continue;
+ if( errno ==22) continue;
       
-        if (strcmp(input, "echo") == 0)
+        if (strcmp(input, "echo") == 0)//בדיקה האם הקלט שווה ל"echo"
             echo(arguments);
-       else if (strncmp(input, "cd", 2) == 0){
+       else if (strncmp(input, "cd", 2) == 0){//בדיקה האם הקלט מתחיל ב"cd"
             cd(arguments);
         }    
-        else if (strcmp(input, "cp") == 0)
+        else if (strcmp(input, "cp") == 0)//בדיקה האם הקלט שווה ל"cp"
             cp(arguments);
+        free(arguments);
+        free(input);
         if(strcmp(arguments[0], "read") == 0){
             readFile(arguments);
         }
-        else if(strcmp(arguments[0], "echo") == 0){
+        else if(strcmp(arguments[0], "echo") == 0){//בדיקה האם המחרוזת הראשונה במערך היא "echo"
             echorite(arguments);
         }
-        else if(strcmp(arguments[0], "wc") == 0){
+        else if(strcmp(arguments[0], "wc") == 0){//בדיקה האם המחרוזת הראשונה במערך היא "wc"
             wordCount(arguments);
         }
-        else if(strcmp(arguments[0], "echo") == 0){
+        else if(strcmp(arguments[0], "echo") == 0){//בדיקה האם המחרוזת הראשונה במערך היא "echo"
             echoppend(arguments);
         }
-        else if(strcmp(arguments[0], "mv") == 0){
+        else if(strcmp(arguments[0], "mv") == 0){//בדיקה האם המחרוזת הראשונה במערך היא "mv"
             move(arguments);
         }
-        else if(strcmp(arguments[0], "delete") == 0){
+        else if(strcmp(arguments[0], "delete") == 0){//בדיקה האם המחרוזת הראשונה במערך היא "delete"
             delete(arguments);
         }
-        else if(strcmp(arguments[0], "dir") == 0){
+        else if(strcmp(arguments[0], "dir") == 0){//בדיקה האם המחרוזת הראשונה במערך היא "dir"
             get_dir();
         }
-         else if (piping)
+         else if (piping)//בדיקה האם יש פייפ
         {
-            mypipe(splitArgumentsArr[0], splitArgumentsArr[1]);
+            // indexPipe
+            // arguments[indexPipe]=NULL;
+            // mypipe(arguments, arguments+indexPipe+1);
+            mypipe(arguments, arguments+/*indexPipe*/+1);
             wait(NULL);
             for(int i = 0; splitArgumentsArr[i]; i++){
                 free(splitArgumentsArr[i]);
@@ -103,8 +108,8 @@ char ***splitArgumentsArr = splitByPipe(arguments);
         }
         else
         {
-            systemCall(arguments);
-            wait(NULL);
+            systemCall(arguments);//קריאה לפונקציה להפעלת תוכניות
+            wait(NULL);//המתנה לסיום התוכנית
         }
         free(arguments);
         free(input);
@@ -114,7 +119,7 @@ char ***splitArgumentsArr = splitByPipe(arguments);
 
 
 void welcome() {
-    printf("Welcome to my Shell\n");
+    printf("Welcome to my Shell\n");//הדפסת הקדימות
           printf("GitHub Username:\n");
         printf("__   __       _ _     _             \n");
         printf("\\ \\ / /__  __| (_) __| |_   _  __ _ \n");
